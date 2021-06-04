9bff5ab9f514a011a00dff1dd1f36358b191d6a9
maokabe <>
2021-06-05 01:21:46 +0900

    implement action and action creater

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+
+//Action Creater
+//returnは省略
+export const increment = () => {{
+        type: 'INCREMENT'
+    }}
+
+export const increment = () => {{
+        type: 'DECREMENT'
+    }}
